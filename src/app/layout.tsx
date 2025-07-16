03d209328a18e3ed61a69b40de3d3fe101f99757
Marcio Jarros <marciojarros@hotmail.com>
2025-07-16 17:36:40 -0300

    added robots and SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const josefin = Josefin_Sans({
 });
 
 export const metadata: Metadata = {
-  title: `Calculadora de IMC (Índice de Massa Corporal) - Adultos, Idosos e Crianças`,
+  title: `Calculadora de IMC - Adultos, Idosos e Crianças`,
 };
 
 export default function RootLayout({
@@ -20,6 +20,13 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <meta name="google-site-verification" content="jMe4B3UjZdywbjepRqkW4fytr0BgmNnIbDJmf5Nnl4Q" />
+        <meta name="description" content="Calculadora de IMC para Adultos, Idosos e Criancas." />
+        <meta name="keywords" content="imc, calculadora imc, imc adulto, imc bebe, imc idoso, imc crianca, indice massa corporal" />
+        <link rel="canonical" href="https://calculadora-imc-adultos-criancas-idos.vercel.app" />
+
+        <meta property="og:title" content="Calculadora de IMC - Adultos, Idosos e Criancas." />
+        <meta property="og:description" content="Calculadora de IMC para Adultos, Idosos e Criancas." />
+        <meta property="og:url" content="https://calculadora-imc-adultos-criancas-idos.vercel.app" />
       </head>
       <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>{children}</body>
     </html>
