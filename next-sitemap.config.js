f618edbf8a1a899a1b831ca97074e9b9c6e15b3a
Marcio Jarros <marciojarros@hotmail.com>
2025-07-20 13:56:45 -0300

    added sitemap function for next

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  siteUrl: "https://calculadora-imc-adultos-criancas-idos.vercel.app",
-  generateRobotsTxt: true, // (optional) automatically generate robots.txt
-  // Optional: additional config to customize crawling, exclude pages, etc.
-};
